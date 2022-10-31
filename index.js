e059e3d5676eaa2d45378adbe22af7c1b9cdae9e
ankit <bankarankit3@gmail.com>
2022-10-31 08:59:31 +0530

    js file added

---
+++ index.js
@@ -0,0 +1,127 @@
+// // ..................variable........................
+// let a=10;       //declare variable  here value 10 stored in a
+// let  b=20;
+// // console.log("sum is",a+b)    // sum of two number
+
+// // ...................operators..........................
+// // 1)addittion(+)
+// console.log("sum is", a + b)
+// // 2)substract(-)
+// console.log("sum is", a -b)
+// // 3)multiply(*)
+// console.log("sum is", a * b)
+// // 3)modules(%)
+// console.log("sum is", a % b)
+// // 3)divide(/)
+// console.log("sum is", a / b)
+
+
+
+// .......................function..............................
+
+// function add(){
+//     const a=5,b=2;
+//     console.log(a+b)
+// }
+// add()
+
+// function add(a,b){
+//     console.log(a+b)
+// }
+// console.log(add(2,6))
+
+// ...................Array.............
+//array is collection of diff. data types its store multiple value in single variable
+
+// let arr = ["Ankit","abhishek","Arjun"] 
+// Array methods
+// reverse,pop,push,shift,unshit,sort
+// arr.pop()
+// console.log(arr)             //removes last element of array
+
+// reverse()
+// arr.reverse()
+// console.log(arr)         //REVERSE ARRAY ELEMENTS
+
+
+// push()
+// arr.push("monika")
+// console.log(arr) 
+
+// arr.sort()                  //sort array elements alphabetica order
+// console.log(arr) 
+
+// .......foreach...
+// let numbers = [1, 3, 4, 9, 8];
+
+// function sqr(number){
+//     console.log(number*number)
+// }
+// numbers.forEach(sqr)
+
+
+// .........................obj.......................
+// const arr = {
+//     name:'Ankit',
+//     age:29,
+//     education:"bca"
+// }
+// console.log(arr.name)
+
+// ......array of object........
+// const students=[{                        //array of object
+//     name:"ANkit",
+//     age:20,
+//     edu:"bca"
+// },{
+//     name: "mukesh",
+//     age: 18,
+//     edu: "12"
+// },{
+
+//     name: "abhishek",
+//     age: 19,
+//     edu: "10"
+
+// }]
+// console.log(arr[0]['name'])
+// students.map((student)=>{
+//     console.log(student)
+// })
+
+
+// ***************LOOPS***************
+// let a=10;
+// if(i=10){
+//     console.log("true")
+// }else{
+//     console.log("false")
+// }
+
+// while loop**************************
+// for(let i=1;i<=10;i++){      //table of 2 using while
+//     console.log(i*2)
+// }
+
+//ifelsif
+// let i=12;
+// if(i=2){
+//     console.log("false")
+// }
+// else if(i=9){
+//     console.log("false")
+// }
+// else if(i=11){
+//     console.log("false")
+// }
+// else{
+//     console.log("no one condition is true")
+// }
+
+
+let a=20;
+if(a=30 && 10 ){
+   console.log("true")
+}else{
+    console.log("false")
+}
