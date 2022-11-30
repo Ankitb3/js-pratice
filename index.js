8dae01a7475970d57e2cc7b39086e08db1954462
ankit <bankarankit3@gmail.com>
2022-11-30 08:57:12 +0530

    added

--- index.js
+++ index.js
@@ -51,6 +51,21 @@
 // arr.sort()                  //sort array elements alphabetica order
 // console.log(arr) 
 
+
+//splice 
+// const arr = ["jan","feb","march","jun","july"]
+// arr.splice(5,0,"aug")
+// console.log(arr)
+
+//slice it create new array from array also it take two agr(1,3) but first one not include    ex:
+// const arr = ["jan", "feb", "march"]
+// const newarray = arr.slice(1,2)
+
+// const arr =["jan","feb","march"]
+// const newarray=arr.slice(0)
+// console.log(arr)
+//  console.log(newarray)
+
 // .......foreach...
 // let numbers = [1, 3, 4, 9, 8];
 
@@ -58,8 +73,6 @@
 //     console.log(number*number)
 // }
 // numbers.forEach(sqr)
-
-
 // .........................obj.......................
 // const arr = {
 //     name:'Ankit',
@@ -156,12 +169,12 @@
 
 
 
- 
+
 //    function ngpw(){
 //     setTimeout(() => {
 //          console.log("21")
 //      }, 3000)
-    
+
 //  }
 //    function punew(){
 
@@ -171,7 +184,7 @@
 //  }
 
 
- 
+
 // *****************json**********************
 // json means js object notation
 
@@ -201,7 +214,7 @@
 //     console.log("hello my name is" + " " + student["name"])
 //   })
 
-  //****using map*** */
+//****using map*** */
 // students.map((student) => {
 //     //  console.log(student["name"],student["fess"])
 //     console.log("hello my name is" + " " + student["name"])
@@ -215,3 +228,227 @@
 // let b = "im web devloper"
 // console.log(`${a+b}`)
 
+// **************promises*************
+
+// let mypromise = new Promise(function(resolve,rejecect){
+//      resolve()
+//      rejecect()
+// })
+// mypromise.then({
+//       function(value){
+//         console.log("sucess")
+//       },
+//       function(err){
+//         console.log("you got err")
+//       }
+
+// })
+
+
+// *********array of object******
+// const datas = [{
+//   name:"Ankit",profession:"Software engg."
+// },{
+//   name: "mukesh", profession: "Software engg."
+// }];
+// function getdatas(){
+//   setTimeout(() => {
+//     datas.forEach((data)=>{
+//       console.log(data.name)
+//     })
+
+//   }, 1000);
+// }
+// function createdata(newdata){
+//   setTimeout(() => {
+//         datas.push(newdata)
+//   }, 1000);
+// }
+// getdatas()
+// createdata({name:"abhishek",profession:"software engg."})
+
+//for in loop in array
+//get index number
+// let myfriends = ["ankit","abhishek","arjun","rohit"]
+// for(let elements in myfriends){
+//   console.log(elements)
+// }
+
+
+//for of loop in array
+//get elements
+// let myfriends = ["ankit", "abhishek", "arjun", "rohit"]
+//  for(let elements of myfriends){
+//   console.log(elements)
+// }
+
+
+//foreach in array
+//call function foreach in array
+
+
+
+// **********async/await********
+
+// async function main(){
+//   const add = new promise((resolve)=>{
+//     resolve("sucess")
+//   })
+// }
+// main.then(
+//     console.log("success")
+// )
+
+
+//map and reduce method
+
+// const array1=[1,4,9,16,25];
+// // num>9
+// let newArr= array1.map((currentelement,index,arr)=>{
+//     return currentelement>9;
+// })
+// console.log(array1)
+// console.log(newArr)
+
+//return new array without changing of old array
+// let newarr =array1.map((ele,index,arr)=>{
+//     return `index no = ${index} and the value is ${ele} belong to ${arr}`
+// })
+// console.log(newarr)
+
+
+
+
+//challenge time 
+//1.find sq root of an each element in an  array
+//sol:-
+// let arr = [25,36,49,64,81];
+// let sqrtt = arr.map((curEle)=>{
+//     return Math.sqrt(curEle);    //sqrt method
+// })
+// console.log(sqrtt)
+
+
+
+//2 multipky each ellemnt by 2 and return only those elemtns whivh are greater than 10?
+//sol:
+// let arr1=[2,3,4,6,8];
+// let arr2=arr1.map((curEle)=>{
+//     return curEle*2;
+// }).filter((curEle)=>{
+//     curEle>10;
+// })
+// console.log(arr2)
+
+
+//Reduce method()
+//convert 3d or 2d array into one D array
+//the reduce() method execute a reducer function  (that yot provide )
+//on each elemnt of array,resulting single outuput.
+//reducer function take four arg
+//Accumlator,value,index,array
+
+
+// let arr =[5,6,2];
+// let sum = arr.reduce((accumaltor,curele)=>{
+//     debugger;
+//     return accumaltor*= curele
+// },7)
+// console.log(sum)
+
+
+
+//Array destructing
+// const favlang=["js","php","python","c"]
+// let[jsv,,,cv]=favlang;
+// console.log(cv)
+
+// how to swap variable
+// let a=10,b=20;
+// [a,b]=[b,a]
+// console.log(a,b)
+
+
+// Object destructuring
+// const biodata={
+//     name:"ankit",
+//     age:29,
+//     deg:"Bca"
+        
+// }
+// let{name,age,deg}=biodata;
+// console.log(`my name is ${name} and age is ${age} and deg is ${deg}`)
+
+
+// Arrow function
+// let a=20,b=10;
+// const sum=()=>{return a+b
+// }
+// console.log(sum())
+
+//default parameter
+// function mult(a,b){
+//     console.log(a*b)
+// }
+// mult(2,1)
+
+
+
+//rest parameter
+// function sum(...inputs){
+//     console.log(...inputs);
+//     let total=0;
+//     for(let i of inputs){
+//         total+=1
+
+//     }
+//     console.log(total)
+// }
+// sum(1,2,3,4,5,6,8,9)
+
+
+
+//Math object  1)sign()  2)trunc()
+// let a=5.77;
+// console.log(Math.sign(a))  
+// console.log(Math.trunc(a))  
+
+
+//number and global method 1)isfinite 2)isNaN  3)isInteger()
+// 1)isFinite
+    // let a=Infinity;
+    // if(isFinite(a)){
+    //     console.log("yes this finite")
+    // }
+    // else{
+    //     console.log("No this is not finite")
+    // }
+
+    // 2)isNaN()
+    //   let a=5;
+    //   console.log(isNaN(a))    output:false
+
+
+    // 3)isInteger()
+    // let a=5;
+    // console.log(Number.isInteger(a))  output:true
+
+
+
+    //oops
+    // class Students{
+    //     constructor(name,age,cls){
+    //         this.myname=name;
+    //         this.myage=age;
+    //         this.mycls=cls;
+    //     }
+    //     biodata(){
+    //         console.log(`${this.myname}  ${this.myage}  ${this.mycls} `)
+    //     }
+    // }
+
+    // class player extends Students {
+    //     constructor(game)
+    //     super(name,age,cls,game);
+    //     this.mygame=game
+    // }
